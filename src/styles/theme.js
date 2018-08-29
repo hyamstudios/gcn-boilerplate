f32775c6a6c09a9c89ec388a02375c7249503a51
ten <ten@hyam.de>
2018-08-29 10:22:08 +0200

    clean up theme & siteConfig

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,12 @@
-const theme = {
+module.exports = {
 	fonts: {
 		sans: 'Helvetica, sans-serif',
 	},
+	colors: {
+		/* Required: For PWA/Offline Settings */
+		backgroundColor: '#FFFFFF',
+		themeColor: '#0000FF',
+		/* The Rest */
+	},
 	fontSizes: [12, 16, 24, 36, 48, 72],
 }
-
-export default theme
--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,18 +1,31 @@
 module.exports = {
-  siteTitle: 'HYAM GCN',
-  siteTitleAlt: 'HYAM GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
-  publisher: 'hy.am studios', // Organization name used for SEO schema
-  siteDescription: '',
+  /* MUST HAVES */
+
   siteUrl: 'https://gcn.netlify.hyamstudios.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  siteTitle: 'HYAM GCN',
+  siteDescription: '',
+
   author: 'hy.am studios', // Author for RSS author segment and SEO schema
   authorUrl: 'https://hyam.de', // URL used for author and publisher schema, can be a social profile or other personal site
-  userTwitter: '@hyamstudios', // Change for Twitter Cards
+
+  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+
+  /* Schema */
+
+  siteTitleAlt: 'HYAM GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
   shortTitle: 'hyam wiki', // Used for App manifest e.g. Mobile Home Screen
+  publisher: 'hy.am studios', // Organization name used for SEO schema
+
+  /* SEO Critical */
+
+  userTwitter: '@hyamstudios', // Change for Twitter Cards
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
-  backgroundColor: '#e9e9e9', // Used for Offline Manifest
-  themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+
+  /* PWA/OFFLINE SETTINGS */
+
+  backgroundColor: require('./../styles/theme.js').colors.backgroundColor, // Used for Offline Manifest
+  themeColor: require('./../styles/theme.js').colors.themeColor, // Used for Offline Manifest
 }
