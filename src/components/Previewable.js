5c94d2a1140ead0019570be94549ccf06758e299
ten <ten@hyam.de>
2018-09-04 10:22:45 +0200

    experimental: Previewable

---
+++ src/components/Previewable.js
@@ -0,0 +1,50 @@
+let Previewable = null
+
+if (process.env.NODE_ENV === 'production') {
+	// No-op
+	Previewable = function(component) {
+		return component
+	}
+} else {
+	const React = require('react')
+	const createClient = require('contentful').createClient
+	const client = createClient({
+		space: process.env.SPACE_ID,
+		accessToken: process.env.ACCESS_TOKEN,
+		host: 'preview.contentful.com',
+	})
+	Previewable = function(Component, query) {
+		return class _Previewable extends React.Component {
+			static displayName = `Previewable(${Component.name ||
+				Component.displayName})`
+			state = {
+				isQueryDone: false,
+				queryData: null,
+			}
+			componentDidMount() {
+				if (query) {
+					query(client, this.props).then(queryData => {
+						this.setState({
+							isQueryDone: !!queryData,
+							queryData,
+						})
+					})
+				}
+			}
+			render() {
+				return (
+					<Component
+						{...this.props}
+						data={
+							this.state.isQueryDone
+								? this.state.queryData
+								: this.props.data
+						}
+					/>
+				)
+			}
+		}
+	}
+}
+
+export default Previewable
--- src/templates/page.js
+++ src/templates/page.js
@@ -5,7 +5,9 @@ import { Box, Heading } from 'rebass'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
-const SectionsTemplate = ({ data }) => {
+import Previewable from '../components/Previewable'
+
+const PageTemplate = ({ data }) => {
   const d = data.page
   return (
     <Layout>
@@ -29,6 +31,7 @@ const SectionsTemplate = ({ data }) => {
 export const query = graphql`
   query($slug: String!) {
     page: contentfulPage(slug: { eq: $slug }) {
+      contentful_id
       title
       slug
       body {
@@ -41,4 +44,18 @@ export const query = graphql`
   }
 `
 
-export default SectionsTemplate
+export default Previewable(PageTemplate, async (client, props) => {
+  const entry = await client.getEntry(props.data.page.contentful_id)
+  const data = {
+    ...props.data,
+  }
+  data.page.title = entry.fields.title
+  data.page.slug = entry.fields.slug
+  data.page.body = {
+    markdown: {
+      html: entry.fields.body,
+      excerpt: entry.fields.body,
+    },
+  }
+  return data
+})
