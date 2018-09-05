5404d494cae8236bd4b74c9cc55c36701b4f2d54
ten <ten@hyam.de>
2018-09-05 10:11:04 +0200

    test 2

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sed -i 's/PLACEHOLDER_SPACE_ID/$SPACE_ID/g' ./netlify.toml
-sed -i 's/PLACEHOLDER_ACCESS_TOKEN/$ACCESS_TOKEN/g' ./netlify.toml
-sed -i 's/PLACEHOLDER_PREVIEW_TOKEN/$PREVIEW_TOKEN/g' ./netlify.toml
-cat ./netlify.toml
+printenv
+# sed -i 's/PLACEHOLDER_SPACE_ID/$SPACE_ID/g' ./netlify.toml
+# sed -i 's/PLACEHOLDER_ACCESS_TOKEN/$ACCESS_TOKEN/g' ./netlify.toml
+# sed -i 's/PLACEHOLDER_PREVIEW_TOKEN/$PREVIEW_TOKEN/g' ./netlify.toml
+# cat ./netlify.toml
 # gatsby build
