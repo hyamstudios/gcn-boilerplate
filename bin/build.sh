65a1ba77fc1299bb80a8fc76bce48f22ec679fbb
ten <ten@hyam.de>
2018-09-05 10:15:17 +0200

    Revert "fix path"
    This reverts commit 84f66f645682e821228ba9c00c4d2e767353345d.
    
    # Conflicts:
    #	bin/build.sh

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-printenv
-# sed -i 's/PLACEHOLDER_SPACE_ID/$SPACE_ID/g' ./netlify.toml
-# sed -i 's/PLACEHOLDER_ACCESS_TOKEN/$ACCESS_TOKEN/g' ./netlify.toml
-# sed -i 's/PLACEHOLDER_PREVIEW_TOKEN/$PREVIEW_TOKEN/g' ./netlify.toml
-# cat ./netlify.toml
-# gatsby build
