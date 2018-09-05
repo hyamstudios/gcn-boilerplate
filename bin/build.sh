84f66f645682e821228ba9c00c4d2e767353345d
ten <ten@hyam.de>
2018-09-05 09:56:26 +0200

    fix path

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed -i 's/PLACEHOLDER_SPACE_ID/$SPACE_ID/g' ./../netlify.toml
-sed -i 's/PLACEHOLDER_ACCESS_TOKEN/$ACCESS_TOKEN/g' ./../netlify.toml
-sed -i 's/PLACEHOLDER_PREVIEW_TOKEN/$PREVIEW_TOKEN/g' ./../netlify.toml
+sed -i 's/PLACEHOLDER_SPACE_ID/$SPACE_ID/g' ./netlify.toml
+sed -i 's/PLACEHOLDER_ACCESS_TOKEN/$ACCESS_TOKEN/g' ./netlify.toml
+sed -i 's/PLACEHOLDER_PREVIEW_TOKEN/$PREVIEW_TOKEN/g' ./netlify.toml
 gatsby build
