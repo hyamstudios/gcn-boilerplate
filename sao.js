2d3a7db1e9e70a741b7a07a01da23522b68a9874
ten <ten@hyam.de>
2018-09-05 16:23:00 +0200

    finalize sao config

--- sao.js
+++ sao.js
@@ -1,13 +1,17 @@
 const fs = require('fs')
 const path = require('path')
 
-const pkgJSON = fs.readFileSync(
-	path.join(__dirname, 'package.json'), 'utf8'
-)
+const pkgJSON = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')
 const pkgObj = JSON.parse(pkgJSON)
 
 module.exports = {
-	templateFolder: __dirname,
+	templateFolder: './',
+	filters: {
+		'.git/**': false,
+		'node_modules/**': false,
+		'docs/**': false,
+		'public/**': false, // just in case...
+	},
 	prompts: {
 		name: {
 			message: 'Name of the project',
@@ -38,9 +42,10 @@ module.exports = {
 		pkgObj.name = answers.name
 		pkgObj.description = answers.description
 		pkgObj.version = answers.version
-		pkgObj.repository = answers.repo === this.prompts.repo.default
-			? this.prompts.repo.default.replace('[folder name]', answers.name)
-			: answers.repo
+		pkgObj.repository =
+			answers.repo === this.prompts.repo.default
+				? this.prompts.repo.default.replace('[name]', answers.name)
+				: answers.repo
 		pkgObj.author = `${answers.user} <${answers.email}>`
 		const str = JSON.stringify(pkgObj, null, 2)
 		return {
@@ -48,6 +53,12 @@ module.exports = {
 		}
 	},
 	post(context, stream) {
-		fs.writeFileSync(path.join(context.folderPath,'package.json'),stream.meta.data.packageJSONContent,'utf8')
+		fs.writeFileSync(
+			path.join(context.folderPath, 'package.json'),
+			stream.meta.data.packageJSONContent,
+			'utf8'
+		)
 	},
+	yarnInstall: true,
+	gitInit: true,
 }
