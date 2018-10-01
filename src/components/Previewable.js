1ad23d43ccb742677e5d14ae2637338989a2d441
ten <ten@hyam.de>
2018-10-01 11:30:29 +0200

    remove the giant remark helper in Previewable.js

--- src/components/Previewable.js
+++ src/components/Previewable.js
@@ -7,53 +7,6 @@ if (__PREVIEW__ENABLED__) {
 		accessToken: __PREVIEW__ACCESS_TOKEN__,
 		host: 'preview.contentful.com',
 	})
-	const _ = require('lodash')
-	const Remark = require('remark')
-	const select = require(`unist-util-select`)
-	const toHAST = require(`mdast-util-to-hast`)
-	const hastToHTML = require(`hast-util-to-html`)
-	const sanitizeHTML = require(`sanitize-html`)
-	const remark = new Remark()
-	const helpers = {
-		/***
-			remark() - replicates the fields from gatsby-transformer-remark
-		****/
-		remark: (input, { fields, excerptLength = 140 }) => {
-			const need = v => fields.indexOf(v) > -1
-			const htmlAst = toHAST(remark.parse(input))
-			const html = hastToHTML(htmlAst)
-			const headings = select(htmlAst, `heading`).map(heading => {
-				return {
-					value: _.first(
-						select(heading, `text`).map(text => text.value)
-					),
-					depth: heading.depth,
-				}
-			})
-			const pureText = sanitizeHTML(html, {
-				allowedTags: [],
-			})
-			const wordCount = _.words(pureText).length
-			const timeToRead = wordCount / 265
-			const excerpt = _.truncate(pureText, {
-				length: excerptLength,
-				omission: `…`,
-			}).replace('\n', ' ')
-			const obj = {}
-			need('html') && (obj['html'] = html)
-			need('htmlAst') && (obj['htmlAst'] = htmlAst)
-			need('headings') && (obj['headings'] = headings)
-			need('excerpt') && (obj['excerpt'] = excerpt)
-			need('timeToRead') && (obj['timeToRead'] = timeToRead)
-			need('wordCount') && (obj['wordCount'] = wordCount)
-			need('tableOfContents') &&
-				console.warn(
-					'tableOfContents field is not supported in preview'
-				)
-			need('tableOfContents') && (obj['tableOfContents'] = null)
-			return obj
-		},
-	}
 	Previewable = function(Component, query) {
 		return class _Previewable extends React.Component {
 			static displayName = `Previewable(${Component.name ||
@@ -71,7 +24,6 @@ if (__PREVIEW__ENABLED__) {
 					const queryData = await query({
 						client,
 						props: this.props,
-						helpers,
 					})
 					this.setState({
 						isQueryDone: !!queryData,
