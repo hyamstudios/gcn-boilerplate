8183be54146514f89f7fae09d3fbcb2074ac72f5
ten <ten@hyam.de>
2018-10-16 17:01:58 +0200

    tryout react-spring by a comparision view

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -1,21 +1,36 @@
-import React, { Fragment } from 'react'
-import { StaggeredMotion, Motion, spring } from 'react-motion'
-
+import React from 'react'
+import { Spring, Trail } from 'react-spring'
 import { Button, Flex, Box, Text, Heading } from 'rebass'
-
 import Layout from 'components/Layout'
 
 class MotionExample extends React.Component {
   state = {
-    x: 0,
-    y: 0,
-    config: {
-      stiffness: 230,
-      damping: 26,
+    position: {
+      x: 0,
+      y: 0,
     },
+    config: {
+      tension: 230,
+      friction: 26,
+      overshootClamping: true,
+      restDisplacementThreshold: 0.1,
+      restSpeedThreshold: 0.1,
+    },
+    items: new Array(10).fill(0).map((_, i) => i),
     mouse: { x: 0, y: 0 },
   }
   $mousePool = React.createRef()
+  updateMouse = e => {
+    this.setState({
+      mouse: {
+        x: Math.min(
+          this.$mousePool.current.offsetWidth - 50,
+          e.pageX - this.$mousePool.current.offsetLeft - 25
+        ),
+        y: Math.min(550, e.pageY - this.$mousePool.current.offsetTop - 25),
+      },
+    })
+  }
   render() {
     return (
       <Layout>
@@ -26,8 +41,10 @@ class MotionExample extends React.Component {
                 mr={0.5}
                 onClick={() => {
                   this.setState({
-                    x: Math.random() * 600,
-                    y: Math.random() * 300,
+                    position: {
+                      x: Math.random() * 600,
+                      y: Math.random() * 300,
+                    },
                   })
                 }}
               >
@@ -36,49 +53,51 @@ class MotionExample extends React.Component {
               <Button
                 onClick={() => {
                   this.setState({
-                    x: this.state.x === 0 ? 500 : 0,
-                    y: 0,
+                    position: {
+                      x: this.state.position.x === 0 ? 500 : 0,
+                      y: 0,
+                    },
                   })
                 }}
               >
-                {this.state.x === 0 ? 'x500y0' : 'x0y0'}
+                {this.state.position.x === 0 ? 'x500y0' : 'x0y0'}
               </Button>
             </Box>
             <Box width={1 / 3}>
-              <Heading fontSize={0}>stiffness</Heading>
-              <Text>{this.state.config.stiffness}</Text>
+              <Heading fontSize={0}>tension</Heading>
+              <Text>{this.state.config.tension}</Text>
               <Box
                 width={0.8}
                 as="input"
                 type="range"
                 min="1"
                 max="500"
-                value={this.state.config.stiffness}
+                value={this.state.config.tension}
                 onChange={e => {
                   this.setState({
                     config: {
                       ...this.state.config,
-                      stiffness: parseFloat(e.target.value),
+                      tension: parseFloat(e.target.value),
                     },
                   })
                 }}
               />
             </Box>
             <Box width={1 / 3}>
-              <Heading fontSize={0}>damping</Heading>
-              <Text>{this.state.config.damping}</Text>
+              <Heading fontSize={0}>friction</Heading>
+              <Text>{this.state.config.friction}</Text>
               <Box
                 width={0.8}
                 as="input"
                 type="range"
                 min="1"
                 max="500"
-                value={this.state.config.damping}
+                value={this.state.config.friction}
                 onChange={e => {
                   this.setState({
                     config: {
                       ...this.state.config,
-                      damping: parseFloat(e.target.value),
+                      friction: parseFloat(e.target.value),
                     },
                   })
                 }}
@@ -86,32 +105,24 @@ class MotionExample extends React.Component {
             </Box>
           </Flex>
           <Box bg="#333333" style={{ height: 300 }} mb={10}>
-            <Motion
-              defaultStyle={{ x: 200, y: 200 }}
-              style={{
-                x: spring(this.state.x, this.state.config),
-                y: spring(this.state.y, this.state.config),
-              }}
+            <Spring
+              config={this.state.config}
+              from={{ x: 500, y: 0 }}
+              to={this.state.position}
             >
-              {value => {
-                return (
-                  <Box
-                    css="width:90px;height:90px;"
-                    my={3}
-                    p={1}
-                    bg="red"
-                    color="white"
-                    style={{
-                      transform: `translate(${value.x}px,${value.y}px)`,
-                    }}
-                  >
-                    {/* <Text>{value.x.toFixed(1)}</Text>
-                    <Text>{'  |  '}</Text>
-                    <Text>{value.y.toFixed(1)}</Text> */}
-                  </Box>
-                )
-              }}
-            </Motion>
+              {value => (
+                <Box
+                  css="width:90px;height:90px;"
+                  my={3}
+                  p={1}
+                  bg="red"
+                  color="white"
+                  style={{
+                    transform: `translate(${value.x}px,${value.y}px)`,
+                  }}
+                />
+              )}
+            </Spring>
           </Box>
           <Box
             ref={this.$mousePool}
@@ -134,72 +145,39 @@ class MotionExample extends React.Component {
               })
             }}
           >
-            <StaggeredMotion
-              defaultStyles={new Array(10).fill(0).map(() => ({ x: 0, y: 0 }))}
-              styles={prevStyles =>
-                prevStyles.map((_, i) => {
-                  const config = this.state.config
-                  return i === 0
-                    ? {
-                        x: spring(this.state.mouse.x, config),
-                        y: spring(this.state.mouse.y, config),
-                      }
-                    : {
-                        x: spring(
-                          Math.max(
-                            0,
-                            Math.min(
-                              this.$mousePool.current.offsetWidth - 50,
-                              prevStyles[i - 1].x + 50
-                            )
-                          ),
-                          config
-                        ),
-                        y: spring(
-                          Math.max(
-                            0,
-                            Math.min(
-                              this.$mousePool.current.offsetHeight - 50,
-                              prevStyles[i - 1].y
-                            )
-                          ),
-                          config
-                        ),
-                      }
-                })
-              }
+            <Trail
+              config={this.state.config}
+              from={{ x: 500, y: 0 }}
+              to={this.state.mouse}
+              keys={this.state.items.map((_, i) => i)}
             >
-              {interpolations => (
-                <Fragment>
-                  {interpolations.map((data, i) => (
-                    <Box
-                      css="position:absolute;width:50px;height:50px;border-radius:50%"
-                      key={i}
-                      style={{
-                        background: `hsla(0,0%,${((1 + i) / 5) * 100}%,1)`,
-                        transform: `translate(${Math.max(
-                          0,
-                          Math.min(
-                            this.$mousePool.current
-                              ? this.$mousePool.current.offsetWidth - 50
-                              : 0,
-                            data.x
-                          )
-                        )}px,${Math.max(
-                          0,
-                          Math.min(
-                            this.$mousePool.current
-                              ? this.$mousePool.current.offsetHeight - 50
-                              : 0,
-                            data.y
-                          )
-                        )}px)`,
-                      }}
-                    />
-                  ))}
-                </Fragment>
-              )}
-            </StaggeredMotion>
+              {this.state.items.map((_, i) => data => (
+                <Box
+                  css="position:absolute;width:50px;height:50px;border-radius:50%"
+                  key={i}
+                  style={{
+                    background: `hsla(0,0%,${((1 + i) / 5) * 100}%,1)`,
+                    transform: `translate(${Math.max(
+                      0,
+                      Math.min(
+                        this.$mousePool.current
+                          ? this.$mousePool.current.offsetWidth - 50
+                          : 0,
+                        data.x + i * 50
+                      )
+                    )}px,${Math.max(
+                      0,
+                      Math.min(
+                        this.$mousePool.current
+                          ? this.$mousePool.current.offsetHeight - 50
+                          : 0,
+                        data.y
+                      )
+                    )}px)`,
+                  }}
+                />
+              ))}
+            </Trail>
           </Box>
         </Box>
       </Layout>
