0e81e042d8b0071e8591d9e9a702a47ce784c9d8
ten <ten@hyam.de>
2019-05-07 16:03:31 +0200

    fix(netlify form): implementation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Field, Formik, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
 export default () => {
   const [formSubmitted, setFormSubmitted] = useState(false);
@@ -18,8 +19,20 @@ export default () => {
         email: '',
         message: '',
       }}
-      onSubmit={() => {
-        setFormSubmitted(true);
+      onSubmit={async (values, actions) => {
+        actions.setStatus({ submitError: null });
+        try {
+          await axios({
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: JSON.stringify(values),
+            url: '/',
+          });
+          setFormSubmitted(true);
+        } catch (error) {
+          actions.setStatus({ submitError: error.message });
+          actions.setSubmitting(false);
+        }
       }}
       validationSchema={Yup.object().shape({
         name: Yup.string().required('Please enter your name'),
@@ -29,28 +42,23 @@ export default () => {
         message: Yup.string().required('Please leave us a message'),
       })}
     >
-      {({ isValid, isSubmitting }) => (
-        <Form name="contact" method="POST" data-netlify>
+      {({ isValid, isSubmitting, status }) => (
+        <Form name="contact" data-netlify="true" noValidate>
           <fieldset>
             <legend>contact detail</legend>
-            <label htmlFor="name">
-              Name
-              <Field required type="text" name="name" />
-            </label>
+            <label htmlFor="name">Name</label>
+            <Field required type="text" name="name" />
             <ErrorMessage name="name" />
-            <label htmlFor="email">
-              Email
-              <Field required type="email" name="email" />
-            </label>
+            <label htmlFor="email">Email</label>
+            <Field required type="email" name="email" />
             <ErrorMessage name="email" />
-            <label htmlFor="message">
-              Message
-              <Field required component="textarea" name="message" />
-            </label>
+            <label htmlFor="message">Message</label>
+            <Field required component="textarea" name="message" />
             <ErrorMessage name="message" />
             <button type="submit" disabled={!isValid || isSubmitting}>
               Submit
             </button>
+            {status && status.submitError && <div>{status.submitError}</div>}
           </fieldset>
         </Form>
       )}
