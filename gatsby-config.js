16fca29cdecc59d9c3db6c32c782c8a3f5dde497
ten <ten@hyam.de>
2019-05-07 11:13:45 +0200

    chore: reduce Contentful binding complexity

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,6 @@
 require('dotenv').config();
 const config = require('./src/utils/siteConfig');
 
-let contentfulConfig;
-
-try {
-  /* eslint-disable-next-line */
-  contentfulConfig = require('./.contentful');
-} catch (e) {
-  contentfulConfig = {
-    production: {
-      host: process.env.CONTENTFUL_HOST,
-      spaceId: process.env.SPACE_ID,
-      accessToken: process.env.GATSBY_PREVIEW === 'true' ? process.env.PREVIEW_TOKEN : process.env.ACCESS_TOKEN,
-    },
-  };
-} finally {
-  const { spaceId, accessToken } = contentfulConfig.production;
-  if (!spaceId || !accessToken) {
-    /* eslint-disable-next-line */
-    throw new Error('Contentful space ID and access token need to be provided.');
-  }
-}
-
 module.exports = {
   // Note: it must *not* have a trailing slash.
   // pathPrefix: `/example`,
@@ -48,7 +27,11 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-contentful',
-      options: process.env.NODE_ENV === 'development' ? contentfulConfig.development : contentfulConfig.production,
+      options: {
+        host: process.env.CONTENTFUL_HOST,
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
