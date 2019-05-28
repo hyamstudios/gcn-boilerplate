dc8c27cd066ab6d7d7c378dbe05c8e3b166034a1
ten <ten@mutate.space>
2019-05-28 10:26:23 +0200

    docs: improve documentation and code quality on some components

--- src/components/Multiline.js
+++ src/components/Multiline.js
@@ -1,7 +1,15 @@
 import React, { Fragment } from 'react';
 
-const Multiline = props => {
-  const a = props.children.split('\n').map((s, index, array) => (
+/**
+ *
+ * @param {{ children: string }} props
+ *
+ * This component render multiline strings into multiline texts in HTML
+ * by adding <br /> to the end of every line.
+ *
+ */
+const Multiline = ({ children }) => {
+  const a = children.split('\n').map((s, index, array) => (
     <Fragment key={s}>
       {s}
       {index === array.length - 1 ? false : <br />}
